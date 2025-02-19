c27778649bd38730e501c583ebcf2131921ec891
Jose Raul <raulserena01@gmail.com>
2025-02-19 18:43:48 +0100

    Plugin try menu

--- Index.php
+++ Index.php
@@ -1,13 +1,15 @@
-<?php
-$titulo='Dropcoding'; 
-?>
+<?php 
+/**
+ * Plugin Name: FAQer 
+ * Description: This plugin is a demo for a FAQ 
+ * Version: 1.0.0
+ * Author: Raul
+ * License: GPL2
+ */
 
-<!Doctype html>
-<html lang="en">
-<head>
- <meta charset="UFTP-8">
-</head>
-<body>
-    <h1>Hola fernando</h1>
-</body>
-</html>
+ add_action('wp_footer', function(){?>
+
+<script>
+	// Here comes the code
+</script>
+<?php }  ,9999); ?>
