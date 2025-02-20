b8c366b1e9a57bcc71ad749f1b7eb080f2dddf4a
Jose Raul <raulserena01@gmail.com>
2025-02-20 11:48:55 +0100

    FInally menu and submenu

--- includes/fqr-functions.php
+++ includes/fqr-functions.php
@@ -6,16 +6,16 @@ function fqr_Add_My_Admin_Link()
 {
     add_menu_page(
         'Miau Primera Pagina', // Title of the page
-        'FAQer plugin_raul', // Text to show on the menu link
+        'FAQer Menu', // Text to show on the menu link
         'manage_options', // Capability requirement to see the link
         'FAQer', // Slug del menú (URL amigable)
-        'faqer_page', // Función que mostrará el contenido de la página
+        'faqr_page', // Función que mostrará el contenido de la página
         'dashicons-admin-plugins', // Icono del menú (puedes usar uno de los iconos predeterminados de WordPress)
         4 // Posición en el menú de administración
     );
 
     add_submenu_page(
-        'fqr-plugin',           // El slug del menú principal al que pertenece
+        'FAQer',           // El slug del menú principal al que pertenece
         'Submenú FAQer', // Título de la página del submenú
         'Submenú',              // Nombre del submenú que aparecerá en el menú
         'manage_options',       // Permiso requerido
