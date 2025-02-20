ea57c1a3a21c4e0d416cb55139223b83d069b0ea
Jose Raul <raulserena01@gmail.com>
2025-02-20 19:01:55 +0100

    newfaqs and newcategoria base finish

--- includes/fqr-new-categoria.php
+++ includes/fqr-new-categoria.php
@@ -1,13 +1,27 @@
 <?php
 function faqer_new_categoria_page() {
     ?> 
-    <div class="wrap">
-        <h1>Bienvenido al apartado para crear nuevas categorias</h1>
-        <p>Esta es la página de categoria donde creas nuevas categorias.</p>
-        <form method="post" action="">
-            <p>Ahora no hace nada, solo testing</p>
-            <input type="submit" value="Guardar Cambios" class="button-primary">
-        </form>
-    </div>
+   <div class="wrap">
+    <h1>Crear Nueva Categoría</h1>
+    <form method="post" action="">
+        <?php
+        // Configuración del editor
+        $contenido_por_defecto = ''; 
+        $editor_id = 'descripcion_categoria';
+
+        $configuracion_editor = array(
+            'textarea_name' => 'descripcion_categoria', // Nombre del campo en el formulario
+            'media_buttons' => true, // Habilita el botón "Añadir medios"
+            'teeny' => false, // Usa la versión completa del editor
+            'quicktags' => true // Habilita etiquetas rápidas (negrita, cursiva, etc.)
+        );
+
+        // Muestra el editor en el formulario
+        wp_editor($contenido_por_defecto, $editor_id, $configuracion_editor);
+        ?>
+        <br>
+        <input type="submit" value="Guardar Categoría" class="button button-primary">
+    </form>
+</div>
     <?php
 }
--- includes/fqr-nuevo-faq.php
+++ includes/fqr-nuevo-faq.php
@@ -1,13 +1,27 @@
 <?php
 function faqer_new_faq_page() {
     ?> 
-    <div class="wrap">
-        <h1>Bienvenido al apartado para crear FAQs desde 0</h1>
-        <p>Esta es la página para crear y editar los faqs</p>
-        <form method="post" action="">
-            <p>Ahora no hace nada, solo testing</p>
-            <input type="submit" value="Guardar Cambios" class="button-primary">
-        </form>
-    </div>
+   <div class="wrap">
+    <h1>Crear Nuevo FAQs</h1>
+    <form method="post" action="">
+        <?php
+        // Configuración del editor
+        $contenido_por_defecto = ''; 
+        $editor_id = 'descripcion_FAQ';
+
+        $configuracion_editor = array(
+            'textarea_name' => 'descripcion_FAQ', // Nombre del campo en el formulario
+            'media_buttons' => true, // Habilita el botón "Añadir medios"
+            'teeny' => false, // Usa la versión completa del editor
+            'quicktags' => true // Habilita etiquetas rápidas (negrita, cursiva, etc.)
+        );
+
+        // Muestra el editor en el formularios
+        wp_editor($contenido_por_defecto, $editor_id, $configuracion_editor);
+        ?>
+        <br>
+        <input type="submit" value="Guardar FAQ" class="button button-primary">
+    </form>
+</div>
     <?php
 }
