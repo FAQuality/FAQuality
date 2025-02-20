691e49908aaf5c309787e0986620ebd8dad62d95
Jose Raul <raulserena01@gmail.com>
2025-02-20 12:55:24 +0100

    All submenus creates

--- includes/fqr-functions.php
+++ includes/fqr-functions.php
@@ -4,19 +4,24 @@ include 'fqr-primera-pagina.php';
 include 'fqr-categoria.php';
 include 'fqr-new-categoria.php';
 include 'fqr-faq.php';
+include 'fqr-nuevo-faq.php';
+include 'fqr-contacto.php';
+include 'fqr-aboutus.php';
 
 // Add a new top level menu link to the ACP
 function fqr_Add_My_Admin_Link()
 {
     add_menu_page( //Menu principal
         'Miau Primera Pagina', // Title of the page
-        'FAQer Menu', // Text to show on the menu link
+        'FAQer_raul_edition', // Text to show on the menu link
         'manage_options', // Capability requirement to see the link
         'FAQer', // Slug del menú (URL amigable)
         'faqer_page', // Función que mostrará el contenido de la página
         'dashicons-admin-plugins', // Icono del menú (puedes usar uno de los iconos predeterminados de WordPress)
         4 // Posición en el menú de administración
     );
+
+
     add_submenu_page( //Menu categoria
         'FAQer',           // El slug del menú principal al que pertenece
         'Categoria', // Título de la página del submenú
@@ -34,12 +39,36 @@ function fqr_Add_My_Admin_Link()
         'faqer_new_categoria_page' // Función que renderiza la página del submenú
     );
     add_submenu_page( //Menu crear categorias
-        'FAQer',           // El slug del menú principal al que pertenece
+        'FAQer',   // El slug del menú principal al que pertenece
         'FAQ', // Título de la página del submenú
         'FAQ',  // Nombre del submenú que aparecerá en el menú
-        'manage_options',       // Permiso requerido
+        'manage_options',  // Permiso requerido
         'FAQ',   // Slug único para la página del submenú
         'faqer_faq_page' // Función que renderiza la página del submenú
+    );
+    add_submenu_page( //Menu crear faqs
+        'FAQer',  // El slug del menú principal al que pertenece
+        'Nuevo FAQ', // Título de la página del submenú
+        'Nuevo FAQ',  // Nombre del submenú que aparecerá en el menú
+        'manage_options',       // Permiso requerido
+        'Nuevo_FAQ',   // Slug único para la página del submenú
+        'faqer_new_faq_page' // Función que renderiza la página del submenú
+    );
+    add_submenu_page( //Menu contacto
+        'FAQer',  // El slug del menú principal al que pertenece
+        'Contacto', // Título de la página del submenú
+        'Contato',  // Nombre del submenú que aparecerá en el menú
+        'manage_options',       // Permiso requerido
+        'Contacto',   // Slug único para la página del submenú
+        'faqer_contact_page' // Función que renderiza la página del submenú
+    );
+    add_submenu_page( //Menu contacto
+        'FAQer',  // El slug del menú principal al que pertenece
+        'About_Us', // Título de la página del submenú
+        'About Us',  // Nombre del submenú que aparecerá en el menú
+        'manage_options',       // Permiso requerido
+        'aboutus',   // Slug único para la página del submenú
+        'faqer_aboutus_page' // Función que renderiza la página del submenú
     );
 }
 
---
+++ includes/fqr-nuevo-faq.php
@@ -0,0 +1,13 @@
+<?php
+function faqer_new_faq_page() {
+    ?> 
+    <div class="wrap">
+        <h1>Bienvenido al apartado para crear FAQs desde 0</h1>
+        <p>Esta es la página para crear y editar los faqs</p>
+        <form method="post" action="">
+            <p>Ahora no hace nada, solo testing</p>
+            <input type="submit" value="Guardar Cambios" class="button-primary">
+        </form>
+    </div>
+    <?php
+}
