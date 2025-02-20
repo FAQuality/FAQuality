f84136d1abb19f9569f8b51c6893d3cba347ff92
miaudona <donanferyt@gmail.com>
2025-02-20 09:54:35 +0100

    Fixing page function

--- includes/fqr-functions.php
+++ includes/fqr-functions.php
@@ -1,6 +1,6 @@
 <?php
 add_action( 'admin_menu', 'fqr_Add_My_Admin_Link' );
-
+include 'fqr-primera-pagina.php'
 // Add a new top level menu link to the ACP
 function fqr_Add_My_Admin_Link()
 {
@@ -9,7 +9,7 @@ function fqr_Add_My_Admin_Link()
         'FAQer plugin', // Text to show on the menu link
         'manage_options', // Capability requirement to see the link
         'FAQer', // Slug del menú (URL amigable)
-        'FAQer_pagina', // Función que mostrará el contenido de la página
+        'faqer_page()', // Función que mostrará el contenido de la página
         'dashicons-admin-plugins', // Icono del menú (puedes usar uno de los iconos predeterminados de WordPress)
         4 // Posición en el menú de administración
     );
@@ -21,18 +21,4 @@ add_shortcode('fqr_shortcode', 'fqr_shortcode_function');
 function fqr_shortcode_function() {
     return '<p>Este es un shortcode de FQR Plugin.</p>';
 }
-function mi_plugin_pagina() {
-    ?> 
-    <div class="wrap">
-        <h1>Bienvenido a Mi Plugin</h1>
-        <p>Esta es la página de configuración de tu plugin personalizado.</p>
-        <form method="post" action="options.php">
-            <?php
-            settings_fields('mi_plugin_opciones'); // Asegúrate de usar un nombre único para las opciones
-            do_settings_sections('mi-plugin'); // Registrar la sección de tu plugin si la necesitas
-            ?>
-            <input type="submit" value="Guardar Cambios" class="button-primary">
-        </form>
-    </div>
-    <?php
-}
+
--- includes/fqr-primera-pagina.php
+++ includes/fqr-primera-pagina.php
@@ -1,4 +1,16 @@
-<div class="wrap">
-  <h1>Hello!</h1>
-  <p>This is my plugin's first page</p>
-</div>
+<?php
+function faqer_page() {
+    ?> 
+    <div class="wrap">
+        <h1>Bienvenido a Mi Plugin</h1>
+        <p>Esta es la página de configuración de tu plugin personalizado.</p>
+        <form method="post" action="options.php">
+            <?php
+            settings_fields('faqer_options'); // Asegúrate de usar un nombre único para las opciones
+            do_settings_sections('faqer-plugin'); // Registrar la sección de tu plugin si la necesitas
+            ?>
+            <input type="submit" value="Guardar Cambios" class="button-primary">
+        </form>
+    </div>
+    <?php
+}
