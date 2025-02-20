abfb7f555587eb4e77adf8842613e79e3794f5e5
Jose Raul <raulserena01@gmail.com>
2025-02-20 10:49:32 +0100

    New branche with submenu

--- includes/fqr-functions.php
+++ includes/fqr-functions.php
@@ -1,8 +1,6 @@
 <?php
-include 'fqr-primera-pagina.php';
-
 add_action( 'admin_menu', 'fqr_Add_My_Admin_Link' );
-
+include 'fqr-primera-pagina.php';
 // Add a new top level menu link to the ACP
 function fqr_Add_My_Admin_Link()
 {
@@ -11,9 +9,18 @@ function fqr_Add_My_Admin_Link()
         'FAQer plugin', // Text to show on the menu link
         'manage_options', // Capability requirement to see the link
         'FAQer', // Slug del menú (URL amigable)
-        'faqer_page', // Función que mostrará el contenido de la página
+        'fqr_plugin_page', // Función que mostrará el contenido de la página
         'dashicons-admin-plugins', // Icono del menú (puedes usar uno de los iconos predeterminados de WordPress)
         4 // Posición en el menú de administración
+    );
+
+    add_submenu_page(
+        'fqr-plugin',           // El slug del menú principal al que pertenece
+        'Submenú FAQer', // Título de la página del submenú
+        'Submenú',              // Nombre del submenú que aparecerá en el menú
+        'manage_options',       // Permiso requerido
+        'fqr-plugin-submenu',   // Slug único para la página del submenú
+        'fqr_plugin_submenu_page' // Función que renderiza la página del submenú
     );
 }
 
@@ -22,4 +29,10 @@ add_shortcode('fqr_shortcode', 'fqr_shortcode_function');
 
 function fqr_shortcode_function() {
     return '<p>Este es un shortcode de FQR Plugin.</p>';
+}
+function fqr_plugin_submenu_page() {
+    echo '<div class="wrap">';
+    echo '<h1>Bienvenido al Submenú de Mi Plugin</h1>';
+    echo '<p>Aquí va el contenido de la página del submenú.</p>';
+    echo '</div>';
 }
