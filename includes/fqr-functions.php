4e6b475b6ba9174deb88c40e3d3303ac6021d6c2
Jose Raul <raulserena01@gmail.com>
2025-02-20 19:55:25 +0100

    icon and position change

--- includes/fqr-functions.php
+++ includes/fqr-functions.php
@@ -17,8 +17,8 @@ function fqr_Add_My_Admin_Link()
         'manage_options', // Capability requirement to see the link
         'FAQer', // Slug del menú (URL amigable)
         'faqer_page', // Función que mostrará el contenido de la página
-        'dashicons-admin-plugins', // Icono del menú (puedes usar uno de los iconos predeterminados de WordPress)
-        4 // Posición en el menú de administración
+        'dashicons-format-status', // Icono del menú (puedes usar uno de los iconos predeterminados de WordPress)
+        8 // Posición en el menú de administración
     );
 
 
