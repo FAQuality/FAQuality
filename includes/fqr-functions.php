d5a10f0b42a5bbbba9332d2f6a92921bcee88e77
miaudona <donanferyt@gmail.com>
2025-02-20 10:00:01 +0100

    added ;

--- includes/fqr-functions.php
+++ includes/fqr-functions.php
@@ -1,6 +1,6 @@
 <?php
 add_action( 'admin_menu', 'fqr_Add_My_Admin_Link' );
-include 'fqr-primera-pagina.php'
+include 'fqr-primera-pagina.php';
 // Add a new top level menu link to the ACP
 function fqr_Add_My_Admin_Link()
 {
