d64bb32df19efd44cec018815ea86047832926a9
miaudona <donanferyt@gmail.com>
2025-02-26 12:46:08 +0100

    ya funciona la creacin de base de datos al activar el plugin

--- fqr-plugin.php
+++ fqr-plugin.php
@@ -1,14 +1,11 @@
 <?php
 /*
-Plugin Name: FAQer
-Description: This is my first plugin! It makes a new admin menu link!
+Plugin Name: FAQer Fenrando
+Description: Ooo
 Author: yo
 */
-
-error_log("Llega el codigo al main");
 
 require_once plugin_dir_path(__FILE__) . 'includes/fqr-functions.php';
 require_once plugin_dir_path(__FILE__) . 'init-faqer-2.php';
 
-
-register_activation_hook(__FILE__,"crear_tabla_faq"); // no funciona
+register_activation_hook(__FILE__,"activation"); // no funciona
--- init-faqer-2.php
+++ init-faqer-2.php
@@ -1,4 +1,7 @@
 <?php
+
+require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
 global $wpdb;
 $prefijo = $wpdb->prefix . 'fqr_';
 $tabla_faq = $prefijo . 'faq';
@@ -8,13 +11,14 @@ $PK_categoria = $tabla_categoria . '(id)';
 $PK_faq = $tabla_faq . '(id)';
 $PK_contacto = $tabla_contacto . '(id)';
 
-error_log("Llega el codigo al init");
 // Pregunta, respuesta, categoria, padre, borrado?
 function crear_tabla_faq() {
     global $wpdb;
-    global $tabla_faq;
-    global $PK_faq;
-    global $PK_categoria;
+    $prefijo = $wpdb->prefix . 'fqr_';
+    $tabla_faq = $prefijo . 'faq';
+    $PK_faq = $tabla_faq . '(id)';
+    $tabla_categoria = $prefijo . 'categoria';
+    $PK_categoria = $tabla_categoria . '(id)';
 
     $sql_query = " CREATE TABLE $tabla_faq (
         id INT PRIMARY KEY AUTO_INCREMENT,
@@ -22,54 +26,41 @@ function crear_tabla_faq() {
         respuesta TEXT NOT NULL,
         FK_idcat INT,
         FK_idpadre INT,
-        borrado TINYINT(1) DEFAULT 0 NOT NULL,
-        CHECK (borrado = 0 OR borrado = 1),
+        borrado TINYINT(1) DEFAULT 0 NOT NULL CHECK (borrado = 0 OR borrado = 1),
         FOREIGN KEY (FK_idcat) REFERENCES $PK_categoria,
-        FOREIGN KEY (FK_idpadre) REFERENCES $PK_faq 
-        ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
-        ;";
+        FOREIGN KEY (FK_idpadre) REFERENCES $PK_faq
+        );";
 
     require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    dbDelta($sql_query);
-
-    $resultado = $wpdb->query("SELECT id FROM $tabla_faq LIMIT 1");
-    if ($resultado === false) {
-        error_log("Error al crear la tabla faq: " . $wpdb->last_error);
-    } else {
-        error_log("Tabla faq creada correctamente.");
-    }
+    $resultadodbdelta = dbDelta($sql_query);
 }
 
 // Categoria, descripcion, borrado?
 function crear_tabla_categoria() {
     global $wpdb;
-    global $tabla_categoria;
+    $prefijo = $wpdb->prefix . 'fqr_';
+    $tabla_categoria = $prefijo . 'categoria';
 
-    $sql_query = " CREATE TABLE $tabla_categoria (
+    $sql_query = "CREATE TABLE $tabla_categoria (
         id INT PRIMARY KEY AUTO_INCREMENT,
         categoria VARCHAR(255) NOT NULL,
         descripcion TEXT,
-        borrado TINYINT(1) DEFAULT 0 NOT NULL,
-        CHECK (borrado = 0 OR borrado = 1)
-        ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
-        ;";
+        borrado TINYINT(1) DEFAULT 0 NOT NULL CHECK (borrado = 0 OR borrado = 1)
+        );";
 
     require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    dbDelta($sql_query);
+    $resultadodbdelta = dbDelta($sql_query);
 
-    $resultado = $wpdb->query("SELECT id FROM $tabla_categoria LIMIT 1");
-    if ($resultado === false) {
-        error_log('Error al crear la tabla categoria: ' . $wpdb->last_error);
-    } else {
-        error_log('Tabla categoria creada correctamente.');
-    }
+    
 }
 
 // Nombre, email, fecha, pregunta, atendido?, borrado?
 function crear_tabla_contacto() {
     global $wpdb;
-    global $tabla_contacto;
-    global $PK_faq;
+    $prefijo = $wpdb->prefix . 'fqr_';
+    $tabla_contacto = $prefijo . 'contacto';
+    $tabla_faq = $prefijo . 'faq';
+    $PK_faq = $tabla_faq . '(id)';
 
 
     $sql_query = " CREATE TABLE $tabla_contacto (
@@ -78,30 +69,21 @@ function crear_tabla_contacto() {
         nombre VARCHAR(255) NOT NULL,
         email VARCHAR(255),
         FK_idfaq INT,
-        estado_atendido TINYINT(1) DEFAULT 0 NOT NULL,
-        borrado TINYINT(1) DEFAULT 0 NOT NULL,
-        CHECK (borrado = 0 OR borrado = 1),
-        CHECK (estado_atendido = 0 OR estado_atendido = 1),
+        estado_atendido TINYINT(1) DEFAULT 0 NOT NULL CHECK (estado_atendido = 0 OR estado_atendido = 1),
+        borrado TINYINT(1) DEFAULT 0 NOT NULL CHECK (borrado = 0 OR borrado = 1),
         FOREIGN KEY (FK_idfaq) REFERENCES $PK_faq 
-        ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
-        ;";
+        );";
 
     require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    dbDelta($sql_query);
-
-    $resultado = $wpdb->query("SELECT id FROM $tabla_contacto LIMIT 1");
-    if ($resultado === false) {
-        error_log('Error al crear la tabla contacto: ' . $wpdb->last_error);
-    } else {
-        error_log('Tabla contacto creada correctamente.');
-    }
+    $resultadodbdelta = dbDelta($sql_query);
 }
 
 // Si se "elimina" algo, comprueba en contacto y faq que los que lo tuvieran como FK se marquen como borrados
 function crear_trigger_al_marcar_borrado_pregunta() {
     global $wpdb;
-    global $tabla_faq;
-    global $tabla_contacto;
+    $prefijo = $wpdb->prefix . 'fqr_';
+    $tabla_contacto = $prefijo . 'contacto';
+    $tabla_faq = $prefijo . 'faq';
 
     $sql_query = "CREATE TRIGGER after_update_faq_trigger
         AFTER UPDATE ON $tabla_faq
@@ -119,7 +101,7 @@ function crear_trigger_al_marcar_borrado_pregunta() {
         END;";
 
     require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    $wpdb->query($sql_query);
+    dbDelta($sql_query);
 
     $resultado = $wpdb->query('SHOW TRIGGERS LIKE after_update_faq_trigger');
     if ($resultado === false) {
@@ -132,8 +114,10 @@ function crear_trigger_al_marcar_borrado_pregunta() {
 // Al "eliminar" una categoria, las preguntas con esa categoría, se "eliminan"
 function crear_trigger_al_marcar_borrado_categoria() {
     global $wpdb;
-    global $tabla_categoria;
-    global $tabla_faq;
+    $prefijo = $wpdb->prefix . 'fqr_';
+    $tabla_categoria = $prefijo . 'categoria';
+    $tabla_faq = $prefijo . 'faq';
+
 
     $sql_query = "CREATE TRIGGER after_update_categoria_trigger
         AFTER UPDATE ON $tabla_categoria
@@ -147,7 +131,7 @@ function crear_trigger_al_marcar_borrado_categoria() {
         END;";
 
     require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    $wpdb->query($sql_query);
+    dbDelta($sql_query);
 
     $resultado = $wpdb->query("SHOW TRIGGERS LIKE 'after_update_categoria_trigger'");
     if ($resultado === false) {
@@ -158,10 +142,10 @@ function crear_trigger_al_marcar_borrado_categoria() {
 }
 
 function activation() {
-    error_log("Se activa el plugin");
+    
     crear_tabla_categoria();
-    crear_tabla_contacto();
     crear_tabla_faq();
-    crear_trigger_al_marcar_borrado_pregunta();
-    crear_trigger_al_marcar_borrado_categoria();
+    crear_tabla_contacto();
+    // crear_trigger_al_marcar_borrado_pregunta();
+    // crear_trigger_al_marcar_borrado_categoria();
 }
