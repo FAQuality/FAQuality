a8360bdbbb652e91f1dec10557122cb20fa49f5c
Jose Raul <raulserena01@gmail.com>
2025-03-05 19:35:49 +0100

    fixed shortcode with a new file

--- fqr-plugin.php
+++ fqr-plugin.php
@@ -9,13 +9,10 @@ Author: Los betas
 
 require_once plugin_dir_path(__FILE__) . 'includes/fqr-functions.php'; // Importar los menús del plugin
 require_once plugin_dir_path(__FILE__) . 'init-faqer.php'; // Importar las funciones que se deben de ejecutar al iniciarse el plugin
-
+require_once plugin_dir_path(__FILE__) . 'includes/fqr-shortcode.php';
 register_activation_hook(__FILE__,"activation"); // Función que se ejecuta al activar el plugin
 
-function fqr_shortcode_function() {
-    return "Fuistes troliado PUTOOO!!!";    
-}add_shortcode('mi_shortcode', 'fqr_shortcode_function');
-
+add_shortcode('mi_shortcode', 'frontend_shortcode');
 
 // Evita el acceso directo
 if (!defined('ABSPATH')) {
---
+++ includes/fqr-shortcode.php
@@ -0,0 +1,6 @@
+<?php
+function frontend_shortcode() {
+    return "Fuistes troliado PUTOOO!!!";    
+    
+}add_shortcode('mi_shortcode', 'frontend_shortcode');
+?>
