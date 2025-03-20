57e46f1475222f8a94b8f7b554af5f8f77bf52d8
Jose Raul <raulserena01@gmail.com>
2025-03-20 12:46:29 +0100

    merequetengue

--- includes/fqr-aboutus.php
+++ includes/fqr-aboutus.php
@@ -1,17 +1,20 @@
 <?php
-function faqer_aboutus_page() {
-    ?> 
+function faqer_aboutus_page()
+{
+    ?>
     <div class="wrap">
-    <div class="content">
-        <h1>Sobre nosotros</h1>
-        <h2>¿Quiénes somos?</h2>
-        <p>Somos dos estudiantes con ganas de aprender y, modestia aparte, increíblemente guapos 
-            (o eso nos decimos para motivarnos). Este plugin es nuestro campo de entrenamiento, 
-            un proyecto en constante evolución donde ponemos a prueba lo que vamos aprendiendo. 
-            No esperes una versión final pronto, porque seguiremos mejorándolo mientras sigamos 
-            descubriendo cosas nuevas. ¡Esperamos que te sea útil tanto como a nosotros nos está 
-            sirviendo para aprender! </p>
-    </div>
+        <div class="content">
+            <h1>Sobre nosotros</h1>
+            <h2>¿Quiénes somos?</h2>
+            <p>
+                Somos dos estudiantes con ganas de aprender y, modestia aparte, increíblemente guapos
+                (o eso nos decimos para motivarnos). Este plugin es nuestro campo de entrenamiento,
+                un proyecto en constante evolución donde ponemos a prueba lo que vamos aprendiendo.
+                No esperes una versión final pronto, porque seguiremos mejorándolo mientras sigamos
+                descubriendo cosas nuevas. ¡Esperamos que te sea útil tanto como a nosotros nos está
+                sirviendo para aprender!
+            </p>
+        </div>
     </div>
     <?php
 }
